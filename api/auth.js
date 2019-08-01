52ec587bb3bab43e750ac382cb49df91de82f7bb
BrannanC <brannanconrad@gmail.com>
2019-08-01 11:41:23 -0500

    Auth with jsonwebtokens

---
+++ api/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const privateKey = process.env.PK;
+
+module.exports = async (req, res, next) => {
+  const token = req.get('Authorization');
+  if (!token) {
+    req.isAuth = false;
+    return next();
+  }
+  let decodedToken;
+  try {
+    decodedToken = await jwt.verify(token, privateKey);
+  } catch (err) {
+    req.isAuth = false;
+    return next();
+  }
+  if (!decodedToken) {
+    req.isAuth = false;
+    return next();
+  }
+  req.isAuth = true;
+  req.roleId = decodedToken.roleId;
+  req.userId = decodedToken.userId;
+  next();
+};
