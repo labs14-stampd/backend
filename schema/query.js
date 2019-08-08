be2d978bf4ccd8780bee00dc854e1c5920872277
joepound <14357797+joepound@users.noreply.github.com>
2019-08-07 23:09:56 -0600

    Refactor query resolvers to consistently use async/await

--- schema/query.js
+++ schema/query.js
@@ -5,8 +5,7 @@ const Credentials = require('../models/credentialModel.js');
 const { UserType, SchoolDetailsType, CredentialType } = require('./types.js');
 const { txFunc, web3, contract } = require('../web3/web3.js');
 
-
-const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull} = graphql;
+const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -14,152 +13,161 @@ const RootQuery = new GraphQLObjectType({
     getAllUsers: {
       type: new GraphQLList(UserType),
       description: 'Gets all users',
-      resolve() {
-        return User.find()
-          .then(res => {
-            if (res) {
-              return res;
-            }
-            return new Error('The users could not be found.');
-          })
-          .catch(() => {
-            return new Error('There was an error completing your request.');
-          });
+      resolve: async () => {
+        try {
+          const res = await User.find();
+          if (res) {
+            return res;
+          }
+          return new Error('The users could not be found.');
+        } catch {
+          return new Error('There was an error completing your request.');
+        }
       }
     },
     getUserById: {
       type: UserType,
       description: 'Gets a user by user ID',
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
+      resolve: async (parent, args) => {
         if (!args.id) {
           return new Error('Please include a user ID and try again.');
         }
-        return User.findById(args.id)
-          .then(res => {
-            if (res) {
-              return res;
-            }
-            return new Error('The user could not be found.');
-          })
-          .catch(() => {
-            return new Error('There was an error completing your request.');
-          });
+
+        try {
+          const res = await User.findById(args.id);
+          if (res) {
+            return res;
+          }
+          return new Error('The user could not be found.');
+        } catch {
+          return new Error('There was an error completing your request.');
+        }
       }
     },
     getAllSchoolDetails: {
       type: new GraphQLList(SchoolDetailsType),
       description: 'Gets all schools',
-      resolve() {
-        return Schools.find()
-          .then(res => {
-            if (res.length) {
-              return res;
-            }
-            return new Error('No schools could be found.');
-          })
-          .catch(() => {
-            return new Error('There was an error completing your request.');
-          });
+      resolve: async () => {
+        try {
+          const res = await Schools.find();
+          if (res.length) {
+            return res;
+          }
+          return new Error('No schools could be found.');
+        } catch {
+          return new Error('There was an error completing your request.');
+        }
       }
     },
     getSchoolDetailsBySchoolId: {
       type: SchoolDetailsType,
       description: 'Gets school by school ID',
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
+      resolve: async (parent, args) => {
         if (!args.id) {
           return new Error('Please include a school details ID and try again.');
         }
-        return Schools.findById(args.id)
-          .then(res => {
-            if (res) {
-              return res;
-            }
-            return new Error('School details could not be found.');
-          })
-          .catch(() => {
-            return new Error('there was an error completing your request.');
-          });
+
+        try {
+          const res = await Schools.findById(args.id);
+          if (res) {
+            return res;
+          }
+          return new Error('School details could not be found.');
+        } catch {
+          return new Error('there was an error completing your request.');
+        }
       }
     },
     getAllCredentials: {
       type: new GraphQLList(CredentialType),
       description: 'Gets all credentials',
-      resolve() {
-        return Credentials.find()
-          .then(res => {
-            if (res.length) {
-              return res;
-            }
-            return new Error('No credentials could be found');
-          })
-          .catch(() => {
-            return new Error('there was an error completing your request.');
-          });
+      resolve: async () => {
+        try {
+          const res = await Credentials.find();
+          if (res.length) {
+            return res;
+          }
+          return new Error('No credentials could be found');
+        } catch {
+          return new Error('there was an error completing your request.');
+        }
       }
     },
     getCredentialById: {
       type: CredentialType,
       description: 'Get a credential by ID',
       args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
+      resolve: async (parent, args) => {
         if (!args.id) {
           return new Error('Please include a credential ID and try again.');
         }
-        return Credentials.findById(args.id)
-          .then(res => {
-            if (res) {
-              return res;
-            }
-            return new Error('Credential with that ID could not be found');
-          })
-          .catch(() => {
-            return new Error('there was an error completing your request.');
-          });
+
+        try {
+          const res = await Credentials.findById(args.id);
+          if (res) {
+            return res;
+          }
+          return new Error('Credential with that ID could not be found');
+        } catch {
+          return new Error('there was an error completing your request.');
+        }
       }
     },
     getCredentialsBySchoolId: {
       type: new GraphQLList(CredentialType),
       description: 'Get all of a schools credentials',
       args: { id: { type: GraphQLID } },
-      async resolve(parent, args) {
+      resolve: async (parent, args) => {
         if (!args.id) {
           return new Error('Please include a school ID and try again.');
         }
-        const school = await User.findById(args.id);
-        if (!school) {
-          return new Error('School with that ID could not be found');
-        }
-        return Credentials.findBy({ schoolId: args.id })
-          .then(res => {
-            if (res) {
-              return res;
-            }
+
+        try {
+          const school = await User.findById(args.id);
+          if (!school) {
             return new Error('School with that ID could not be found');
-          })
-          .catch(() => {
-            return new Error('there was an error completing your request.');
-          });
+          }
+
+          const res = await Credentials.findBy({ schoolId: args.id });
+          if (res) {
+            return res;
+          }
+          return new Error('School with that ID could not be found');
+        } catch {
+          return new Error('there was an error completing your request.');
+        }
       }
-    }, 
-    validateCredential:{
+    },
+    validateCredential: {
       type: CredentialType,
       description: 'Checks that a credential exists and is currently valid',
       args: {
-        id:{
-          type: new GraphQLNonNull(GraphQLID), 
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
           description: 'The unique ID of the credential to be validated'
         }
       },
-      async resolve(parent, args){
-        try{
-          console.log('validate args', args)
-          const {id,txHash,valid,expirationDate,created_at, updated_at, ...cred} = await Credentials.findById(args.id);
+      resolve: async (parent, args) => {
+        console.log('validate args', args);
+
+        try {
+          const {
+            id,
+            txHash,
+            valid,
+            expirationDate,
+            created_at,
+            updated_at,
+            ...cred
+          } = await Credentials.findById(args.id);
           cred.schoolId = cred.schoolId.toString();
           const credHash = web3.utils.sha3(JSON.stringify(cred));
-          const data = await contract.methods.validateCredential(credHash).call();
-          } catch(error){
+          const data = await contract.methods
+            .validateCredential(credHash)
+            .call();
+        } catch (error) {
           return error;
         }
       }
