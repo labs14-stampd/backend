5290e368dcae18796a985fe607d5a9e24f204fa5
cecil john tantay <cjbtantay@gmail.com>
2019-08-14 11:58:53 -0700

    change value of subject in token service and change user.roleId in token to res.roleId

--- schema/mutations/userMutations.js
+++ schema/mutations/userMutations.js
@@ -52,7 +52,7 @@ module.exports = {
             token = jwt({
               userId: res.id,
               email: res.email,
-              roleId: user.roleId
+              roleId: res.roleId
             });
             return {
               ...res,
