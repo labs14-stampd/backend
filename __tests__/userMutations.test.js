a3e1718880cb507be708aeb6873b66cd4546d5fa
joepound <14357797+joepound@users.noreply.github.com>
2019-08-14 14:35:51 -0600

    Add additional comments for addUser test case and tidy up code

--- __tests__/userMutations.test.js
+++ __tests__/userMutations.test.js
@@ -10,39 +10,39 @@ afterEach(() => {
 });
 
 describe('• addUser mutation: ', () => {
-  it('• should return the expected data from adding a user that does not yet exist in the database', async () => {
+  it('• should return the expected data when adding a user that does not yet exist in the database', async () => {
     const EXPECTED_USER_ID = 9;
     const EXPECTED_EMAIL = 'johndoe@ymail.com';
     const EXPECTED_USERNAME = 'johndoe';
     const EXPECTED_ROLE_ID = 2;
 
     const MUTATION = `
-    mutation {
-      addUser(
-        authToken: "${process.env.TEST_TOKEN}"
-        roleId: ${EXPECTED_ROLE_ID}
-      ) {
-        id
-        email
-        username
-        roleId
-        token
+      mutation {
+        addUser(
+          authToken: "${process.env.TEST_TOKEN}"
+          roleId: ${EXPECTED_ROLE_ID}
+        ) {
+          id
+          email
+          username
+          roleId
+          token
+        }
       }
-    }
     `;
     const res = await graphql(schema, MUTATION, null);
 
     const actual = res.data.addUser;
 
-    const actualDecodedToken = decoded(actual.token);
+    const decodedActualToken = decoded(actual.token);
 
-    expect(actual.id).toBe(`${EXPECTED_USER_ID}`);
+    expect(actual.id).toBe(EXPECTED_USER_ID.toString()); // the GraphQL response object will have String-type ID's
     expect(actual.email).toBe(EXPECTED_EMAIL);
     expect(actual.username).toBe(EXPECTED_USERNAME);
-    expect(actual.roleId).toBe(`${EXPECTED_ROLE_ID}`);
+    expect(actual.roleId).toBe(EXPECTED_ROLE_ID.toString()); // the GraphQL response object will have String-type ID's
 
-    expect(actualDecodedToken.subject).toBe(EXPECTED_USER_ID);
-    expect(actualDecodedToken.email).toBe(EXPECTED_EMAIL);
-    expect(actualDecodedToken.roleId).toBe(EXPECTED_ROLE_ID);
+    expect(decodedActualToken.subject).toBe(EXPECTED_USER_ID);
+    expect(decodedActualToken.email).toBe(EXPECTED_EMAIL);
+    expect(decodedActualToken.roleId).toBe(EXPECTED_ROLE_ID);
   });
 });
