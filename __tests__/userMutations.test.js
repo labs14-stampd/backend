c8c9197361017b4440b7b8ec79f16547641b50d6
joepound <14357797+joepound@users.noreply.github.com>
2019-08-14 18:11:07 -0600

    Add tests for updateUser GQL mutation
    updateUser mutation:
    • should return the expected data when updating a user's information
    • should actually update the corresponding user's information in the database (without affecting other fields)
    
    Error handling:
    • when "id" parameter is missing
    • when attempting to update a non-existent user

--- __tests__/userMutations.test.js
+++ __tests__/userMutations.test.js
@@ -10,12 +10,15 @@ const cleanUpSeeds = () => db.seed.run();
 beforeAll(cleanUpSeeds);
 afterEach(cleanUpSeeds);
 
+// should be based on current seed data
+const ROLE_COUNT = 4;
+const USER_COUNT = 8;
+
 describe('• addUser GQL mutation: ', () => {
   const EXPECTED_NEW_USER_ID = 9; // derived from seeds (ID value auto-increments)
   const EXPECTED_EMAIL = 'johndoe@ymail.com';
   const EXPECTED_USERNAME = 'johndoe';
 
-  const EXPECTED_ROLE_COUNT = 4; // based on role seeds
   let expectedNewUserRoleID; // role ID to use for each addUser mutation test will be randomly generated
 
   // This addUser mutation will be run in some manner before every test using a provided role ID as a parameter
@@ -36,7 +39,7 @@ describe('• addUser GQL mutation: ', () => {
 
   beforeEach(() => {
     // A randomly generated valid role ID will be used to insert a new user in every test within this scope
-    expectedNewUserRoleID = Math.ceil(Math.random() * EXPECTED_ROLE_COUNT);
+    expectedNewUserRoleID = Math.ceil(Math.random() * ROLE_COUNT);
   });
 
   it('• should return the expected data when adding a user that does not yet exist in the database', async () => {
@@ -65,8 +68,8 @@ describe('• addUser GQL mutation: ', () => {
       addUserMutationUsingRoleID(expectedNewUserRoleID),
       null
     );
-    const actualNewUser = await dbHelper.findById(EXPECTED_NEW_USER_ID); // DB helper method to find the newly added user by ID
 
+    const actualNewUser = await dbHelper.findById(EXPECTED_NEW_USER_ID); // DB helper method to find the newly added user by ID
     expect(actualNewUser.id).toBe(EXPECTED_NEW_USER_ID);
     expect(actualNewUser.username).toBe(EXPECTED_USERNAME);
     expect(actualNewUser.email).toBe(EXPECTED_EMAIL);
@@ -140,5 +143,117 @@ describe('addUser GQL mutation error handling: ', () => {
     expect(res.errors[0].message.startsWith(EXPECTED_ERROR_MESSAGE_START)).toBe(
       true
     );
+  });
+});
+
+describe('updateUser GQL mutation: ', () => {
+  let expectedUserIdToUpdate; // user to update for each updateUser mutation test will be randomly selected among seed data
+  const EXPECTED_UPDATED_USERNAME = 'newname';
+  const EXPECTED_UPDATED_EMAIL = 'new@latest.next';
+  let expectedUpdatedRoleId;
+
+  beforeEach(() => {
+    // Randomly generate valid test ID's before each test
+    expectedUserIdToUpdate = Math.ceil(Math.random() * ROLE_COUNT);
+    expectedUpdatedRoleId = Math.ceil(Math.random() * ROLE_COUNT);
+  });
+
+  // The mutation string below will be reused for this group of tests
+  const updateUserMutationWithArgs = args => `
+    mutation {
+      updateUser (
+        id: ${args.id}
+        username: "${args.username}"
+        email: "${args.email}"
+        roleId: ${args.roleId}
+      ) {
+        id
+        username
+        email
+        roleId
+      }
+    }
+  `;
+
+  it("• should return the expected data when updating a user's information", async () => {
+    const EXPECTED_UPDATES = {
+      id: expectedUserIdToUpdate,
+      username: EXPECTED_UPDATED_USERNAME,
+      email: EXPECTED_UPDATED_EMAIL,
+      roleId: expectedUpdatedRoleId
+    };
+
+    const res = await graphql(
+      schema,
+      updateUserMutationWithArgs(EXPECTED_UPDATES),
+      null
+    );
+    const actual = res.data.updateUser;
+
+    expect(actual.id).toBe(expectedUserIdToUpdate.toString()); // the GraphQL response object will have String-type ID's
+    expect(actual.username).toBe(EXPECTED_UPDATED_USERNAME);
+    expect(actual.email).toBe(EXPECTED_UPDATED_EMAIL);
+    expect(actual.roleId).toBe(expectedUpdatedRoleId.toString()); // the GraphQL response object will have String-type ID's
+  });
+
+  it("• should actually update the corresponding user's information in the database (without affecting other fields)", async () => {
+    // Get original user data to check that data fields without specified values are not updated
+    const originalUser = await dbHelper.findById(expectedUserIdToUpdate);
+    const EXPECTED_PROFILE_PICTURE = originalUser.profilePicture;
+    const EXPECTED_SUB = originalUser.sub;
+
+    const EXPECTED_UPDATES = {
+      id: expectedUserIdToUpdate,
+      username: EXPECTED_UPDATED_USERNAME,
+      email: EXPECTED_UPDATED_EMAIL,
+      roleId: expectedUpdatedRoleId
+    };
+
+    // Initial mutation to update the user
+    await graphql(schema, updateUserMutationWithArgs(EXPECTED_UPDATES), null);
+
+    const actualUpdatedUser = await dbHelper.findById(expectedUserIdToUpdate);
+    expect(actualUpdatedUser.id).toBe(expectedUserIdToUpdate);
+    expect(actualUpdatedUser.username).toBe(EXPECTED_UPDATED_USERNAME);
+    expect(actualUpdatedUser.email).toBe(EXPECTED_UPDATED_EMAIL);
+    expect(actualUpdatedUser.profilePicture).toBe(EXPECTED_PROFILE_PICTURE);
+    expect(actualUpdatedUser.sub).toBe(EXPECTED_SUB);
+    expect(actualUpdatedUser.roleId).toBe(expectedUpdatedRoleId);
+  });
+});
+
+describe('updateUser GQL mutation error handling: ', () => {
+  test('• when "id" parameter is missing', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Please include a user ID and try again.';
+
+    const MUTATION = `
+      mutation {
+        updateUser {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.updateUser).toBeNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when attempting to update a non-existent user', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'User with the given ID not found';
+
+    const MUTATION = `
+      mutation {
+        updateUser (
+          id: 0
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.updateUser).toBeNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
   });
 });
