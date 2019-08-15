4aa56076ee640704b4308da982528c34a5d1bbbc
cecil john tantay <cjbtantay@gmail.com>
2019-08-15 13:49:37 -0700

    test studentDetails Model update

--- __tests__/studentModel.test.js
+++ __tests__/studentModel.test.js
@@ -2,23 +2,19 @@ const db = require('../database/dbConfig');
 const StudentDetails = require('./../models/studentModel');
 const User = require('../models/userModel');
 
+beforeAll( async () => {
+  await db.seed.run();
+});
+
 describe('StudentDetails Model', () => {
-  beforeAll(async () => {
-    await db.seed.run();
-    const user = await User.insert({
-      username: 'testUser',
-      email: 'testUser@test.com',
-      sub: 'testUser'
-    });
-    // testUser with an id of 9
-    return user;
-  });
-  afterEach(() => {
-    return db('users').delete();
-  });
   describe('insert', () => {
     beforeEach(async () => {
-      await StudentDetails.insert({
+      const user = await User.insert({
+        username: 'testUser',
+        email: 'testUser@test.com',
+        sub: 'testUser'
+      });
+      const student = {
         fullName: 'Test User',
         firstName: 'Test',
         middleName: 'D',
@@ -29,15 +25,71 @@ describe('StudentDetails Model', () => {
         state: 'TE',
         zip: '00000',
         phone: '(000)000-0000',
-        userId: 9
-      });
+        userId: user.id
+      }
+
+      await StudentDetails.insert(student);
     });
     afterEach(() => {
-      return db('studentDetails').delete();
+      return db('users').delete();
     });
-    it('shoud have a fullName of Test User', async () => {
+    it('shoud check whether all properties are inserted correctly', async () => {
       const [student] = await StudentDetails.findBy({ firstName: 'Test' });
-      expect(student.fullName).toBe("Test User")
-    })
+      expect(student.fullName).toBe('Test User');
+      expect(student.firstName).toBe('Test');
+      expect(student.middleName).toBe('D');
+      expect(student.lastName).toBe('User');
+      expect(student.street1).toBe('Test St.');
+      expect(student.street2).toBeNull();
+      expect(student.city).toBe('Test');
+      expect(student.state).toBe('TE');
+      expect(student.zip).toBe('00000');
+      expect(student.phone).toBe('(000)000-0000');
+      expect(student.userId).toBeGreaterThan(1);
+      expect(student.length).toBeUndefined();
+    });
+  });
+  describe('update', () => {
+    beforeEach(async () => {
+      const user = await User.insert({
+        username: 'testUser',
+        email: 'testUser@test.com',
+        sub: 'testUser'
+      });
+
+      const student = {
+        fullName: 'Test User',
+        firstName: 'Test',
+        middleName: 'D',
+        lastName: 'User',
+        street1: 'Test St.',
+        street2: null,
+        city: 'Test',
+        state: 'TE',
+        zip: '00000',
+        phone: '(000)000-0000',
+        userId: user.id
+      }
+
+      const updateStudent = {
+        fullName: 'Test Updated',
+        firstName: 'Test',
+        middleName: 'S',
+        lastName: 'Updated'
+      }
+
+      await StudentDetails.insert(student);
+      await StudentDetails.update(user.id, updateStudent)
+    });
+    afterEach(() => {
+      return db('users').delete();
+    });
+    it('shoud check whether properties can be updated', async () => {
+      const [student] = await StudentDetails.findBy({ firstName: 'Test' });
+      expect(student.fullName).toBe('Test Updated');
+      expect(student.firstName).toBe('Test');
+      expect(student.middleName).toBe('S');
+      expect(student.lastName).toBe('Updated');
+    });
   });
 });
