16dd905288b55063f2104d17b3c5309d78551e0f
cecil john tantay <cjbtantay@gmail.com>
2019-08-15 12:32:58 -0700

    test studentDetails insert if the right full name exists

--- __tests__/studentModel.test.js
+++ __tests__/studentModel.test.js
@@ -1,3 +1,43 @@
 const db = require('../database/dbConfig');
-const Students = require('./../models/studentModel');
-const Auth = require('../../auth/authModel');
+const StudentDetails = require('./../models/studentModel');
+const User = require('../models/userModel');
+
+describe('StudentDetails Model', () => {
+  beforeAll(async () => {
+    await db.seed.run();
+    const user = await User.insert({
+      username: 'testUser',
+      email: 'testUser@test.com',
+      sub: 'testUser'
+    });
+    // testUser with an id of 9
+    return user;
+  });
+  afterEach(() => {
+    return db('users').delete();
+  });
+  describe('insert', () => {
+    beforeEach(async () => {
+      await StudentDetails.insert({
+        fullName: 'Test User',
+        firstName: 'Test',
+        middleName: 'D',
+        lastName: 'User',
+        street1: 'Test St.',
+        street2: null,
+        city: 'Test',
+        state: 'TE',
+        zip: '00000',
+        phone: '(000)000-0000',
+        userId: 9
+      });
+    });
+    afterEach(() => {
+      return db('studentDetails').delete();
+    });
+    it('shoud have a fullName of Test User', async () => {
+      const [student] = await StudentDetails.findBy({ firstName: 'Test' });
+      expect(student.fullName).toBe("Test User")
+    })
+  });
+});
