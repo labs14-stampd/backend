cadb4e92dd861752823756b75552bf1b4ee4b3b9
joepound <14357797+joepound@users.noreply.github.com>
2019-08-19 15:13:54 -0600

    Set test suites to run serially
    Run test suites serially to prevent overlapping of seeds (which will erroneously fail tests); also delete model test file temporarily and fix import typo

--- __tests__/schoolMutations.test.js
+++ __tests__/schoolMutations.test.js
@@ -2,10 +2,9 @@ const { graphql } = require('graphql');
 const schema = require('../schema/schema');
 const db = require('../database/dbConfig');
 
-const dbHelper = require('../model/schoolModel');
+const dbHelper = require('../models/schoolModel');
 
 // Re-seed before all mutation tests and after each mutation test to ensure that each test will work with a clean set of data
 const cleanUpSeeds = () => db.seed.run();
 beforeAll(cleanUpSeeds);
 afterEach(cleanUpSeeds);
-
