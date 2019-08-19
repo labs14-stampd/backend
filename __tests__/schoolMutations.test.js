bf19c29b00ea1ab0bffeefc82fa83d5cf5310fd6
joepound <14357797+joepound@users.noreply.github.com>
2019-08-19 15:20:55 -0600

    Add modified test hooks in mutation tests
    The new beforeEach and afterAll definitions will account for the now serially run tests

--- __tests__/schoolMutations.test.js
+++ __tests__/schoolMutations.test.js
@@ -4,7 +4,12 @@ const db = require('../database/dbConfig');
 
 const dbHelper = require('../models/schoolModel');
 
-// Re-seed before all mutation tests and after each mutation test to ensure that each test will work with a clean set of data
-const cleanUpSeeds = () => db.seed.run();
-beforeAll(cleanUpSeeds);
-afterEach(cleanUpSeeds);
+beforeEach(async () => {
+  // Re-seed before all mutation tests to ensure that each test will work with a clean set of data
+  await db.seed.run();
+});
+afterAll(async () => {
+  // Re-seed after all tests in this test suite to ensure that the next test suite will work with a clean set of data
+  await db.seed.run();
+  await db.destroy(); // Necessary to prevent connections from not closing (which could eventually clog the Postgres database if left unchecked)
+});
