c13217e03793e979536d8190f2ef7b248260a761
joepound <14357797+joepound@users.noreply.github.com>
2019-08-22 11:57:54 -0600

    Add error handling tests for checking uniqueness of input values
    addUser GQL mutation error handling:
    • when provided username is not unique
    • when provided email is not unique (within "users" table)
    • when provided email is not unique (within "userEmails" table)
    • when provided sub value is not unique
    
    updateUser GQL mutation error handling:
    • when provided email is not unique (within "users" table)
    • when provided email is not unique (within "userEmails" table)
    • when provided sub value is not unique
    
    addUserEmail GQL mutation error handling:
    • when provided email is not unique (within "userEmails" table)
    • when provided email is not unique (within "users" table)
    
    addSchoolDetail GQL mutation error handling:
    • when provided school name is not unique
    • when provided tax ID is not unique
    
    updateSchoolDetail GQL mutation error handling:
    • when provided school name is not unique
    • when provided tax ID is not unique

--- __tests__/userMutations.test.js
+++ __tests__/userMutations.test.js
@@ -16,12 +16,28 @@ afterAll(async () => {
   await db.destroy(); // Necessary to prevent connections from not closing (which could eventually clog the Postgres database if left unchecked)
 });
 
-// should be based on current seed data
+// Should be based on current seed data
 const ROLE_COUNT = 4;
 const USER_COUNT = 9;
 const USEREMAIL_COUNT = 4;
 
 describe('addUser GQL mutation: ', () => {
+  /*
+    Decoded token contents:
+    {
+      "name": "johndoe@ymail.com",
+      "picture": "https://s.gravatar.com/avatar/7b83367dff0ee92c931ae39590fd839d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjo.png",
+      "updated_at": "2019-08-14T15:44:04.256Z",
+      "email": "johndoe@ymail.com",
+      "email_verified": false,
+      "sub": "auth0|5d542c449ad8300dae000bbf",
+      "username": "johndoe",
+      "iat": 1565800780
+    } 
+   */
+  const TEST_AUTH_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiam9obmRvZUB5bWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvN2I4MzM2N2RmZjBlZTkyYzkzMWFlMzk1OTBmZDgzOWQ_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZqby5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOS0wOC0xNFQxNTo0NDowNC4yNTZaIiwiZW1haWwiOiJqb2huZG9lQHltYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwic3ViIjoiYXV0aDB8NWQ1NDJjNDQ5YWQ4MzAwZGFlMDAwYmJmIiwidXNlcm5hbWUiOiJqb2huZG9lIiwiaWF0IjoxNTY1ODAwNzgwfQ.t9OTsUOHmAigoRqIiL1l2bjmnKeW60dTtBEjNBOTINM';
+
   const EXPECTED_NEW_USER_ID = USER_COUNT + 1; // derived from seeds (ID value auto-increments)
   const EXPECTED_EMAIL = 'johndoe@ymail.com';
   const EXPECTED_USERNAME = 'johndoe';
@@ -32,7 +48,7 @@ describe('addUser GQL mutation: ', () => {
   const addUserMutationUsingRoleId = roleId => `
     mutation {
       addUser(
-        authToken: "${process.env.TEST_TOKEN}"
+        authToken: "${TEST_AUTH_TOKEN}"
         roleId: ${roleId}
       ) {
         id
@@ -151,6 +167,181 @@ describe('addUser GQL mutation error handling: ', () => {
       true
     );
   });
+
+  test('• when provided role ID does not point to an existing role', async () => {
+    const EXPECTED_ERROR_MESSAGE_START =
+      'The given role ID does not match any existing role.';
+
+    /*
+      Decoded token contents:
+      {
+        "name": "johndoe@ymail.com",
+        "picture": "https://s.gravatar.com/avatar/7b83367dff0ee92c931ae39590fd839d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjo.png",
+        "updated_at": "2019-08-14T15:44:04.256Z",
+        "email": "johndoe@ymail.com",
+        "email_verified": false,
+        "sub": "auth0|5d542c449ad8300dae000bbf",
+        "username": "johndoe",
+        "iat": 1565800780
+      } 
+     */
+    const TEST_AUTH_TOKEN =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiam9obmRvZUB5bWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvN2I4MzM2N2RmZjBlZTkyYzkzMWFlMzk1OTBmZDgzOWQ_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZqby5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOS0wOC0xNFQxNTo0NDowNC4yNTZaIiwiZW1haWwiOiJqb2huZG9lQHltYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwic3ViIjoiYXV0aDB8NWQ1NDJjNDQ5YWQ4MzAwZGFlMDAwYmJmIiwidXNlcm5hbWUiOiJqb2huZG9lIiwiaWF0IjoxNTY1ODAwNzgwfQ.t9OTsUOHmAigoRqIiL1l2bjmnKeW60dTtBEjNBOTINM';
+    const TEST_NONEXISTENT_ROLE_ID = 0;
+
+    const MUTATION = `
+      mutation {
+        addUser (
+          authToken: "${TEST_AUTH_TOKEN}"
+          roleId: ${TEST_NONEXISTENT_ROLE_ID}
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUser).toBeNull();
+    expect(res.errors[0].message.startsWith(EXPECTED_ERROR_MESSAGE_START)).toBe(
+      true
+    );
+  });
+
+  test('• when provided username is not unique', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Username must be unique';
+
+    /*
+      Decoded token contents:
+      {
+        "name": "johndoe@ymail.com",
+        "picture": "https://s.gravatar.com/avatar/7b83367dff0ee92c931ae39590fd839d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjo.png",
+        "updated_at": "2019-08-14T15:44:04.256Z",
+        "email": "johndoe@ymail.com",
+        "email_verified": false,
+        "sub": "auth0|5d542c449ad8300dae000bbf",
+        "username": "admin",
+        "iat": 1565800780
+      } 
+     */
+    const TEST_AUTH_TOKEN =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiam9obmRvZUB5bWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvN2I4MzM2N2RmZjBlZTkyYzkzMWFlMzk1OTBmZDgzOWQ_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZqby5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOS0wOC0xNFQxNTo0NDowNC4yNTZaIiwiZW1haWwiOiJqb2huZG9lQHltYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwic3ViIjoiYXV0aDB8NWQ1NDJjNDQ5YWQ4MzAwZGFlMDAwYmJmIiwidXNlcm5hbWUiOiJhZG1pbiIsImlhdCI6MTU2NTgwMDc4MH0.ANbiFzHse0r-PlFt6Rsvi4bueS2aphGdimmTIHGORts';
+
+    const MUTATION = `
+      mutation {
+        addUser (
+          authToken: "${TEST_AUTH_TOKEN}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided email is not unique (within "users" table)', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Email address must be unique';
+
+    /*
+      Decoded token contents:
+      {
+        "name": "johndoe@ymail.com",
+        "picture": "https://s.gravatar.com/avatar/7b83367dff0ee92c931ae39590fd839d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjo.png",
+        "updated_at": "2019-08-14T15:44:04.256Z",
+        "email": "skollboii@studytest.edu",
+        "email_verified": false,
+        "sub": "auth0|5d542c449ad8300dae000bbf",
+        "username": "johndoe",
+        "iat": 1565800780
+      } 
+    */
+    const TEST_AUTH_TOKEN =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiam9obmRvZUB5bWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvN2I4MzM2N2RmZjBlZTkyYzkzMWFlMzk1OTBmZDgzOWQ_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZqby5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOS0wOC0xNFQxNTo0NDowNC4yNTZaIiwiZW1haWwiOiJza29sbGJvaWlAc3R1ZHl0ZXN0LmVkdSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwic3ViIjoiYXV0aDB8NWQ1NDJjNDQ5YWQ4MzAwZGFlMDAwYmJmIiwidXNlcm5hbWUiOiJqb2huZG9lIiwiaWF0IjoxNTY1ODAwNzgwfQ.F3SmBxn_tKj2VqRFo2McFFuOn2JM-vOfkXr_xrFdv94';
+
+    const MUTATION = `
+      mutation {
+        addUser (
+          authToken: "${TEST_AUTH_TOKEN}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided email is not unique (within "userEmails" table)', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Email address must be unique';
+
+    /*
+      Decoded token contents:
+      {
+        "name": "johndoe@ymail.com",
+        "picture": "https://s.gravatar.com/avatar/7b83367dff0ee92c931ae39590fd839d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjo.png",
+        "updated_at": "2019-08-14T15:44:04.256Z",
+        "email": "trufflin.waffles@gmail.com",
+        "email_verified": false,
+        "sub": "auth0|5d542c449ad8300dae000bbf",
+        "username": "johndoe",
+        "iat": 1565800780
+      } 
+    */
+    const TEST_AUTH_TOKEN =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiam9obmRvZUB5bWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvN2I4MzM2N2RmZjBlZTkyYzkzMWFlMzk1OTBmZDgzOWQ_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZqby5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOS0wOC0xNFQxNTo0NDowNC4yNTZaIiwiZW1haWwiOiJ0cnVmZmxpbi53YWZmbGVzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwic3ViIjoiYXV0aDB8NWQ1NDJjNDQ5YWQ4MzAwZGFlMDAwYmJmIiwidXNlcm5hbWUiOiJqb2huZG9lIiwiaWF0IjoxNTY1ODAwNzgwfQ.kNo99_NM_t76XWRkwTCae_RDDQ49RmwF_wjZIgx8B8k';
+
+    const MUTATION = `
+      mutation {
+        addUser (
+          authToken: "${TEST_AUTH_TOKEN}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided sub value is not unique', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Sub value must be unique';
+
+    /*
+      Decoded token contents:
+      {
+        "name": "johndoe@ymail.com",
+        "picture": "https://s.gravatar.com/avatar/7b83367dff0ee92c931ae39590fd839d?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fjo.png",
+        "updated_at": "2019-08-14T15:44:04.256Z",
+        "email": "johndoe@ymail.com",
+        "email_verified": false,
+        "sub": "1",
+        "username": "johndoe",
+        "iat": 1565800780
+      }
+     */
+    const TEST_AUTH_TOKEN =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiam9obmRvZUB5bWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvN2I4MzM2N2RmZjBlZTkyYzkzMWFlMzk1OTBmZDgzOWQ_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZqby5wbmciLCJ1cGRhdGVkX2F0IjoiMjAxOS0wOC0xNFQxNTo0NDowNC4yNTZaIiwiZW1haWwiOiJ0cnVmZmxpbi53YWZmbGVzQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwic3ViIjoiMSIsInVzZXJuYW1lIjoiam9obmRvZSIsImlhdCI6MTU2NTgwMDc4MH0.mDNNSIDeRJDChjinJPo8qxMzJ4F8xSwNPMbMvg3HC8c';
+
+    const MUTATION = `
+      mutation {
+        addUser (
+          authToken: "${TEST_AUTH_TOKEN}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
 });
 
 describe('updateUser GQL mutation: ', () => {
@@ -265,6 +456,119 @@ describe('updateUser GQL mutation error handling: ', () => {
     expect(res.data.updateUser).toBeNull();
     expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
   });
+
+  test('• when provided role ID does not point to an existing role', async () => {
+    const EXPECTED_ERROR_MESSAGE_START =
+      'The given role ID does not match any existing role.';
+
+    const TEST_ID_TO_UPDATE = 1;
+    const TEST_NONEXISTENT_ROLE_ID = 0;
+
+    const MUTATION = `
+      mutation {
+        addUser (
+          id: ${TEST_ID_TO_UPDATE}
+          roleId: ${TEST_NONEXISTENT_ROLE_ID}
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUser).toBeNull();
+    expect(res.errors[0].message.startsWith(EXPECTED_ERROR_MESSAGE_START)).toBe(
+      true
+    );
+  });
+
+  test('• when provided username is not unique', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Username must be unique';
+
+    // Should be based on current seed data
+    const TEST_ID_TO_UPDATE = 1;
+    const EXISTING_USERNAME = 'admin2';
+    const MUTATION = `
+      mutation {
+        updateUser (
+          id: ${TEST_ID_TO_UPDATE}
+          username: "${EXISTING_USERNAME}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.updateUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided email is not unique (within "users" table)', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Email address must be unique';
+
+    // Should be based on current seed data
+    const TEST_ID_TO_UPDATE = 1;
+    const EXISTING_EMAIL = 'school@school.edu';
+    const MUTATION = `
+      mutation {
+        updateUser (
+          id: ${TEST_ID_TO_UPDATE}
+          email: "${EXISTING_EMAIL}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.updateUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided email is not unique (within "userEmails" table)', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Email address must be unique';
+
+    // Should be based on current seed data
+    const TEST_ID_TO_UPDATE = 1;
+    const EXISTING_EMAIL = 'learners.learn@study.edu';
+    const MUTATION = `
+      mutation {
+        updateUser (
+          id: ${TEST_ID_TO_UPDATE}
+          email: "${EXISTING_EMAIL}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.updateUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided sub value is not unique', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Sub value must be unique';
+
+    // Should be based on current seed data
+    const TEST_ID_TO_UPDATE = 1;
+    const EXISTING_SUB = '9';
+    const MUTATION = `
+      mutation {
+        updateUser (
+          id: ${TEST_ID_TO_UPDATE}
+          sub: "${EXISTING_SUB}"
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.updateUser).isNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
 });
 
 describe('deleteUser GQL mutation: ', () => {
@@ -372,8 +676,8 @@ describe('addUserEmail GQL mutation: ', () => {
 
   beforeEach(() => {
     // Randomize field values of the incoming user email entry
-    expectedNewUserId = Math.ceil(Math.random() * USER_COUNT);
     expectedValid = Boolean(Math.floor(Math.random() * 2));
+    expectedNewUserId = Math.ceil(Math.random() * USER_COUNT);
   });
 
   // The mutation string below will be reused for this group of tests
@@ -468,6 +772,60 @@ describe('addUserEmail GQL mutation error handling: ', () => {
           email: "test@test.test"
           userId: 0
         } {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUserEmail).toBeNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided email is not unique (within "userEmails" table)', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Email address must be unique';
+
+    // Should be based on current seed data (specifically the email addresses in the "userEmails" table)
+    const EXISTING_EMAIL = 'trufflin.waffles@gmail.com';
+
+    // Randomize field values of the incoming user email entry
+    const expectedValid = Boolean(Math.floor(Math.random() * 2));
+    const expectedNewUserId = Math.ceil(Math.random() * USER_COUNT);
+
+    const MUTATION = `
+      mutation {
+        addUserEmail (
+          email: "${EXISTING_EMAIL}"
+          valid: ${expectedValid}
+          userID: ${expectedNewUserId}
+        ) {
+          id
+        }
+      }
+    `;
+
+    const res = await graphql(schema, MUTATION, null);
+    expect(res.data.addUserEmail).toBeNull();
+    expect(res.errors[0].message).toBe(EXPECTED_ERROR_MESSAGE);
+  });
+
+  test('• when provided email is not unique (within "users" table)', async () => {
+    const EXPECTED_ERROR_MESSAGE = 'Email address must be unique';
+
+    // Should be based on current seed data (specifically the email addresses in the "users" table)
+    const EXISTING_EMAIL = 'notadud@yandex.com';
+
+    // Randomize field values of the incoming user email entry
+    const expectedValid = Boolean(Math.floor(Math.random() * 2));
+    const expectedNewUserId = Math.ceil(Math.random() * USER_COUNT);
+
+    const MUTATION = `
+      mutation {
+        addUserEmail (
+          email: "${EXISTING_EMAIL}"
+          valid: ${expectedValid}
+          userID: ${expectedNewUserId}
+        ) {
           id
         }
       }
