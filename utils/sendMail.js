3ed893df1397b574b7ae8093e2482cf197906968
byronholmes2018 <lshopeful2018@gmail.com>
2019-08-22 15:15:05 -0500

    Added sendMagicLink function to sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -37,6 +37,49 @@ const sendMail = async ({ recipientName, recipientEmail, jwt }) => {
   });
 };
 
+const sendMagicLink = async ({
+  recipientName,
+  recipientEmail,
+  student,
+  jwt
+}) => {
+  // Create a SMTP transporter object
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.NODEMAILER_ADDRESS,
+      pass: process.env.NODEMAILER_PASSWORD
+    }
+  });
+
+  // Message object
+  const message = {
+    // Comma separated list of recipients
+    from: `"Team Stampd" <${process.env.NODEMAILER_ADDRESS}>`,
+    to: `"${recipientName}" <${recipientEmail}>`,
+
+    // Subject of the message
+    subject: `${student}'s credential is ready to verify!`,
+
+    // plaintext body
+    text: `Please click the link below to verify ${student}'s credential:
+            http://localhost:4000/verifyCred/${jwt}
+        `,
+
+    html: `<p><b>Hello</b> Please click the link below to verify ${student}'s credential.</p><br/><a rel='nofollow' href='http://localhost:4000/verifyCred/${jwt}'>VERIFY.</a>`
+  };
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      return err.message;
+    }
+
+    return nodemailer.getTestMessageUrl(info);
+  });
+};
+
 module.exports = {
-  sendMail
+  sendMail,
+  sendMagicLink
 };
