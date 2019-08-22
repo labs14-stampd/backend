946b127a44adcd099216e5a6f146e5d108eeb353
byronholmes2018 <lshopeful2018@gmail.com>
2019-08-22 14:40:44 -0500

    Cleaned up comments from sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,11 +1,6 @@
 const nodemailer = require('nodemailer');
 
 const sendMail = async ({ recipientName, recipientEmail, jwt }) => {
-  // Generate SMTP service account from ethereal.email
-
-  // NB! Store the account object values somewhere if you want
-  // to re-use the same account for future mail deliveries
-
   // Create a SMTP transporter object
 
   const transporter = nodemailer.createTransport({
@@ -30,35 +25,7 @@ const sendMail = async ({ recipientName, recipientEmail, jwt }) => {
             http://localhost:4000/confirmation/${jwt}
         `,
 
-    // HTML body
     html: `<p><b>Hello</b> Please click the link below to confirm your email address.</p><br/><a rel='nofollow' href='http://localhost:4000/confirmation/${jwt}'>CONFIRM.</a>`
-
-    /*
-        // An array of attachments
-        attachments: [
-            // String attachment
-            {
-                filename: '',
-                content: '',
-                contentType: '' // optional, would be detected from the filename
-            },
-
-            // Binary Buffer attachment
-            {
-                filename: 'image.png',
-                content: Buffer.from(
-                    '',
-                    'base64'
-                ),
-
-                cid: 'note@example.com' // should be as unique as possible
-            },
-
-            // File Stream attachment
-            {
-
-            }
-        ] */
   };
 
   transporter.sendMail(message, (err, info) => {
@@ -68,9 +35,6 @@ const sendMail = async ({ recipientName, recipientEmail, jwt }) => {
 
     return nodemailer.getTestMessageUrl(info);
   });
-
-  // only needed when using pooled connections
-  // transporter.close();
 };
 
 module.exports = {
